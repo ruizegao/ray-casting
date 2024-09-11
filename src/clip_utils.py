semi finished clip integration; does not last iteration of kd tree where splitting should not occur; split values may have wrong dimension

src/clip_utils.py
@@ -20,7 +20,7 @@ from typing import Union, Tuple
 def clip_domains(
         x_L: torch.Tensor,
         x_U: torch.Tensor,
-        thresholds: torch.Tensor,
+        thresholds: Union[torch.Tensor, float],
         lA: torch.Tensor,
         dm_lb: Union[torch.Tensor, None] = None,
         lbias: Union[torch.Tensor, None] = None,
@@ -29,14 +29,14 @@ def clip_domains(
     """
     Takes subdomains (or original domain) and shrinks along dimensions to remove verified portions of the input domain
     to remove redundancy and allow for more effective splits.
-    @param x_L:                 The lower bound on the inputs of the subdomains
-    @param x_U:                 The upper bound on the inputs of the subdomains
-    @param thresholds:          The specification threshold where dom_lb > thresholds implies the subdomain is verified
-    @param lA:                  CROWN lA for subdomains
-    @param dm_lb:               The lower bound on the outputs of the domains
-    @param lbias:               CROWN lbias for subdomains. Needed to concretize dm_lb if dm_lb is not given/incorrect
-    @param calculate_dm_lb:     If set to true, dm_lb is assumed to be None or incorrect. lbias is then needed
-    @return:                    The new x_L, x_U
+    :param x_L:                 The lower bound on the inputs of the subdomains
+    :param x_U:                 The upper bound on the inputs of the subdomains
+    :param thresholds:          The specification threshold where dom_lb > thresholds implies the subdomain is verified
+    :param lA:                  CROWN lA for subdomains
+    :param dm_lb:               The lower bound on the outputs of the domains
+    :param lbias:               CROWN lbias for subdomains. Needed to concretize dm_lb if dm_lb is not given/incorrect
+    :param calculate_dm_lb:     If set to true, dm_lb is assumed to be None or incorrect. lbias is then needed
+    :return:                    The new x_L, x_U
     """
     if calculate_dm_lb:
         assert isinstance(lbias, torch.Tensor), "lbias is needed to concretize dm_lb"
@@ -52,6 +52,9 @@ def clip_domains(
     batches, num_spec, input_dim = lA.shape
     x_L = x_L.clone().view(batches, input_dim)
     x_U = x_U.clone().view(batches, input_dim)
+    if isinstance(thresholds, float):
+        # transform thresholds into a Tensor if given as a single float
+        thresholds = torch.full((batches, num_spec), thresholds, dtype=x_L.dtype, device=x_L.device)
     # x_L/x_U shape: (batch, input_dim)
     # lA shape: (batch, num_spec, input_dim)
     # dm_lb shape: (batch, num_spec)

src/crown.py
@@ -5,7 +5,6 @@ from dataclasses import dataclass
 from collections import defaultdict
 
 import numpy as np
-from dask.array import result_type
 
 import utils
 import torch
@@ -18,6 +17,28 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 batch_size_per_iteration = 100000
+
+# === Crown utility methods
+
+def deconstruct_lbias(_x_L, _x_U, _lA, _dm_lb):
+    """
+
+    Given the input region, Crown's lA matrix and domain lower bounds, deconstructing lbias is very trivial.
+
+    :param _x_L:
+    :param _x_U:
+    :param _lA:
+    :param _dm_lb:
+    :return:
+    """
+    _lA = _lA.flatten(2) # (batch, spec_dim, in_dim)
+    xhat_vect = ((_x_U + _x_L) / 2).flatten(1) # (batch, in_dim)
+    xhat_vect = xhat_vect.unsqueeze(2) # (batch, in_dim, 1)
+    eps_vect = ((_x_U - _x_L) / 2).flatten(1) # (batch, in_dim)
+    eps_vect = eps_vect.unsqueeze(2) # (batch, in_dim, 1)
+    dm_lb_vect = _dm_lb.unsqueeze(2) # (batch, spec_dim, 1)
+    _lbias = dm_lb_vect - (_lA.bmm(xhat_vect) - _lA.abs().bmm(eps_vect))
+    return _lbias.squeeze(2) # (batch, spec_dim)
 
 # === Function wrappers
 