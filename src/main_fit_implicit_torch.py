a88c1f690eb07a5d60ac68aeda455b78ab89ef96
jorgejc2 <jorgejc2@illinois.edu>
2024-11-04 19:58:03 -0600

    torch alternative to fitting implicit surfaces is now capable of training and saving networks into the appropirate format; next step is testing with benchmark meshes

--- src/main_fit_implicit_torch.py
+++ src/main_fit_implicit_torch.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -22,14 +24,16 @@ set_t = {
     'device': torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'),
 }
 
+available_activations = [nn.ReLU, nn.ELU, nn.Sigmoid]
+
 to_numpy = lambda x : x.detach().cpu().numpy()  # converts tensors to numpy arrays
 
 class FitSurfaceModel(nn.Module):
-    def __init__(self, lrate: float, loss_fn, fit_mode: str, activation:str='relu', n_layers:int=8, layer_width:int=32):
+    def __init__(self, lrate: float, fit_mode: str, activation:str='relu', n_layers:int=8, layer_width:int=32):
         """
-
+        Constructs a neural network for fitting to an implicit surface. Layers are carefully named as to make it easier
+        to convert the network into an .npz file that can be used for ray-casting.
         :param lrate:       Learning rate
-        :param loss_fn:     Loss function use to udpated optimizable parameters
         :param fit_mode:    If the neural network should be fit for occupancy or sdf
         :param activation:  Activation function to use at nonlinear layers
         :param n_layers:    Number of layers to use in the neural network
@@ -43,31 +47,53 @@ class FitSurfaceModel(nn.Module):
             activation_fn = nn.ReLU()
         elif activation_lower == 'elu':
             activation_fn = nn.ELU()
-        elif activation_lower == 'cos':
-            activation_fn = torch.cos
         else:
             raise ValueError("Activation not recognized. If you wish to use a new activation function, "
                              "feel free to add it to the list in the constructor.")
+        activation_fn_name = activation_fn.__class__.__name__.lower()
 
         # create the network based on the specifications
-        layers = [nn.Linear(3, layer_width), activation_fn]
+        layers = [
+            ('0000_dense', nn.Linear(3, layer_width)),
+            (f'0001_{activation_fn_name}', activation_fn)
+        ]
         for i in range(n_layers - 2):
-            layers.extend([nn.Linear(layer_width, layer_width), activation_fn])
+            layer_count = len(layers)
+            layer_count_formatted = f"{layer_count:04d}_"
+            layer_count_formatted_plus_one = f"{layer_count + 1:04d}_"
+            layers.extend([
+                (layer_count_formatted + 'dense', nn.Linear(layer_width, layer_width)),
+                (f'{layer_count_formatted_plus_one}{activation_fn_name}', activation_fn)
+            ])
         if fit_mode == 'occupancy':
             # binary classification, should be a probability in range (0, 1)
-            layers.extend([nn.Linear(layer_width, 1), nn.Sigmoid()])
+            layer_count = len(layers)
+            layer_count_formatted = f"{layer_count:04d}_"
+            layer_count_formatted_plus_one = f"{layer_count+1:04d}_"
+            sigmoid_fn_name = nn.Sigmoid().__class__.__name__.lower()
+            layers.extend([
+                (layer_count_formatted + 'dense', nn.Linear(layer_width, 1)),
+                (f'{layer_count_formatted_plus_one}{sigmoid_fn_name}', nn.Sigmoid())
+            ])
+            # set loss function to be binary cross entropy
+            self.loss_fn = nn.BCELoss()
         elif fit_mode == 'sdf':
             # regression, last layer is fine being linear
-            layers.append(nn.Linear(layer_width, 1))
+            layer_count = len(layers)
+            layer_count_formatted = f"{layer_count:04d}_"
+            layers.extend([
+                (layer_count_formatted + 'dense', nn.Linear(layer_width, 1))
+            ])
+            # set loss function to be L1 loss
+            self.loss_fn = nn.L1Loss()
         else:
             raise ValueError("fit_mode must be either 'occupancy' or 'sdf'")
-        self.model = nn.Sequential(*layers)
+        # convert layers to OrderedDict
+        layer_dict = OrderedDict(layers)
+        self.model = nn.Sequential(layer_dict)
 
         # set optimizer
         self.optimizer = optim.Adam(self.model.parameters(), lr=lrate)
-
-        # set loss function
-        self.loss_fn = loss_fn
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -241,6 +267,90 @@ def fit_model(
     # return losses and trained network
     return losses, correct_counts, correct_fracs, NetObject
 
+def plot_training_metrics(losses: list[float], correct_fracs: list[float], save_path: Optional[str] = None, display: bool = False):
+    """
+
+    :param losses:
+    :param correct_fracs:
+    :param save_path:
+    :param display:
+    :return:
+    """
+    if save_path is None and not display:
+        return
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    ax1.plot(losses)
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss')
+    ax1.set_title("Training Loss")
+    ax1.grid()
+
+    ax2.plot(correct_fracs)
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Correct Sign %')
+    ax2.set_title("Number of Correct Sign Predictions")
+    ax2.grid()
+
+    plt.tight_layout()
+    if save_path is not None:
+        plt.savefig(save_path)
+    if display:
+        plt.show()
+    else:
+        plt.close()
+
+def save_to_npz(NetObject: FitSurfaceModel, npz_path: str, verbose: bool = False):
+    """
+    Saves the Torch model as a .npz file that can be loaded in by the other ray tracing scripts.
+    :param NetObject:   Neural network object to save
+    :param npz_path:    .npz file path to save the network to
+    :param verbose:     If true, prints additional logging information
+    :return:
+    """
+    # Iterate over model parameters
+    npz_dict = {}
+    if verbose:
+        print("Adding optimizable parameters to the npz dictionary")
+    for name, param in NetObject.named_parameters():
+        split_name = name.split('.')
+        new_base_name = split_name[1].replace('_', '.') + '.'
+        is_weight = split_name[2] == 'weight'
+        weight_name = 'A' if is_weight else 'b'
+        new_key = new_base_name + weight_name
+
+        # convert parameter to numpy array
+        # if parameter is a weight, it must be permuted
+        if len(param.shape) == 2:
+            np_param = to_numpy(param.permute(1, 0))
+        else:
+            np_param = to_numpy(param)
+        npz_dict[new_key] = np_param
+        if verbose:
+            print(f"Parameter Name: {name}")
+            print(f"Parameter Value: {param}")
+            print("-" * 30)
+
+    if verbose:
+        print("Adding activation functions to the npz dictionary")
+    available_activation_names = [n().__class__.__name__.lower() for n in available_activations]
+    for name, layer in NetObject.model._modules.items():
+        split_name = name.split('_')
+        base_name = split_name[1]
+        if base_name in available_activation_names:
+            new_key = name.replace('_', '.') + '._'
+            np_param = np.empty(0)
+            npz_dict[new_key] = np_param
+        if verbose:
+            print(f"Module Name: {name}")
+            print(f"Module Value: {layer}")
+            print("-" * 30)
+
+    if verbose:
+        print(f"Saving network in .npz format with path {npz_path} \nand dictionary with keys \n{npz_dict.keys()}")
+    np.savez(npz_path, **npz_dict)
+
 def main(
         input_file: Optional[str] = None,
         output_file: Optional[str] = None
@@ -300,15 +410,8 @@ def main(
         raise ValueError("output file should end with .npz")
 
     # initialize the network
-    if args.fit_mode == 'occupancy':
-        loss_fn = nn.BCELoss()
-    elif args.fit_mode == 'sdf':
-        loss_fn = nn.L1Loss()
-    else:
-        raise ValueError("unrecognized fit_mode")
     model_params = {
         'lrate': args.lr,
-        'loss_fn': loss_fn,
         'fit_mode': args.fit_mode,
         'activation': args.activation,
         'n_layers': args.n_layers,
@@ -330,39 +433,46 @@ def main(
 
     # train the neural network
     losses, correct_counts, correct_fracs, NetObject = fit_model(NetObject, train_loader, args.fit_mode, args.n_epochs)
+    NetObject.eval()  # set to evaluation mode
 
     # save the neural network in Torch format
+    pth_file = output_file.replace('.npz', '.pth')
+    print(f"Saving model to {pth_file}...")
+    pth_dict = {
+        "state_dict": NetObject.state_dict(),
+        "model_params": model_params,
+    }
+    torch.save(pth_dict, pth_file)
 
     # display results
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    plt_file = output_file.replace('.npz', '.png')
+    plot_training_metrics(losses, correct_fracs, plt_file, True)
 
-    ax1.plot(losses)
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Loss')
-    ax1.set_title("Training Loss")
-    ax1.grid()
 
-    ax2.plot(correct_fracs)
-    ax2.set_xlabel('Epoch')
-    ax2.set_ylabel('Correct Sign %')
-    ax2.set_title("Number of Correct Sign Predictions")
-    ax2.grid()
+    # save neural network in .npz format
+    save_to_npz(NetObject, output_file, True)
 
-    plt.tight_layout()
-    plt.show()
+def load_netobject(pth_file: str) -> FitSurfaceModel:
+    # load in parameters needed to initialize the model
+    pth_dict = torch.load(pth_file)
+    state_dict = pth_dict["state_dict"]
+    model_params = pth_dict["model_params"]
+    NetObject = FitSurfaceModel(**model_params)
+    NetObject.load_state_dict(state_dict)
+    NetObject.eval()
 
-    # save neural network to the specified path
-    # Iterate over model parameters
-    for name, param in NetObject.named_parameters():
-        print(f"Parameter Name: {name}")
-        print(f"Parameter Value: {param}")
-        print("-" * 30)
+    return NetObject
 
-    # save the neural network in .npz format
+def pth_to_npz():
+    pth_file = '/home/jorgejc2/Documents/Research/ray-casting/Thingi10K_Meshes/59340.pth'
+    NetObject = load_netobject(pth_file)
 
+    npz_file = pth_file.replace('.pth', '.npz')
+    save_to_npz(NetObject, npz_file, True)
 
 if __name__ == '__main__':
     main()
+    # pth_to_npz()
     # use_predefined_files = True
     # input_directory = "/home/jorgejc2/Documents/Research/ray-casting/Thingi10K/raw_meshes/"
     # output_directory = "/home/jorgejc2/Documents/Research/ray-casting/sample_inputs/Thingi10K_inputs/"
